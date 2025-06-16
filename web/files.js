7bf25b7463082883b031bec6a6ef372d183f2c67
Artem Zakirullin <artemzr@gmail.com>
2025-06-16 07:55:16 +0300

    fix file exists

--- web/files.js
+++ web/files.js
@@ -638,7 +638,8 @@ async function saveTextFile(path, content) {
         throw new Error("Invalid file name");
     }
 
-    if (!exists([path]) || !await isContentEqual(path, content)) {
+    const fileExists= !await exists([path]);
+    if (fileExists || !await isContentEqual(path, content)) {
         console.log("Hashes do not match, writing file...", path);
         // TODO rem
         const writable = await fileHandle.createWritable();
